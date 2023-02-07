5258c3a22ea5d971619786b7890cb73c80ba73c4
sljico <anessljivo@gmail.com>
2023-02-07 17:06:48 +0100

    initial commit

---
+++ src/layouts/LoginLayouts.tsx
@@ -0,0 +1,20 @@
+// @ts-ignore
+
+export default function LoginLayout({ children }): any {
+  return (
+    <>
+      <div>
+        <main id="main">{children}</main>
+      </div>
+
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+            background-color: red;
+          }
+        `}
+      </style>
+    </>
+  );
+}
