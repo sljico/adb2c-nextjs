c9849b414f742fbc1f3c3a013a2cde944c3f6f0c
sljico <anessljivo@gmail.com>
2023-02-23 10:20:34 +0100

    test

--- src/pages/b2c/signup.tsx
+++ src/pages/b2c/signup.tsx
@@ -34,7 +34,7 @@ export default function Login() {
           </li>
         </div>
       </LoginLayout>
-      <Script id="signup-script" strategy="afterInteractive">
+      <Script id="signup-script" strategy="afterInteractive" async defer>
         {`
           console.log('dada');
           (function () {
