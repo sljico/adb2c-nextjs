ab93eb900bed176640fab789632b375efb8ea5c6
sljico <anessljivo@gmail.com>
2023-02-23 12:00:22 +0100

    test

--- src/pages/b2c/signup.tsx
+++ src/pages/b2c/signup.tsx
@@ -57,7 +57,7 @@ export default function Login() {
 
             setTimeout(() => {
               console.log('timeout ', document.getElementById('test'))
-            }, 5000);
+            }, 0);
             console.log(tc)
             console.log(checkbox)
 
