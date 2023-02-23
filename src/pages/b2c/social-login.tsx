0eac27b10f8b0ab39007cd55b6d0a592f9f6c3c0
sljico <anessljivo@gmail.com>
2023-02-23 15:05:09 +0100

    test

--- src/pages/b2c/social-login.tsx
+++ src/pages/b2c/social-login.tsx
@@ -10,6 +10,47 @@ const SocialLogin: NextPage<unknown> = () => {
           <div id="api" className="b2c-sociallogin"></div>
         </section>
       </div>
+
+      <div id="extension_termsOfUseContentWrapper">
+        <li className="TextBox">
+          <div className="attrEntry">
+            <div className="textarea">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates, nobis repudiandae rerum iure officia aperiam iste
+              excepturi qui quia mollitia neque aspernatur veritatis. Corrupti
+              quia sequi nesciunt nobis accusamus, dolores, pariatur similique,
+              reiciendis accusantium commodi cum est dignissimos adipisci
+              ratione recusandae! Fugiat omnis veniam corporis maiores corrupti!
+              Quaerat, itaque excepturi velit quos quia aliquam eum ab provident
+              repudiandae asperiores voluptate ut incidunt corporis natus. Totam
+              minus voluptatum quod labore enim eveniet optio ratione modi
+              aperiam sunt saepe assumenda quidem tempore maxime fuga tempora
+              officiis, deleniti quaerat sapiente vel. Doloribus dolorem
+              accusantium perspiciatis commodi. Ea excepturi placeat ipsa
+              accusamus sequi autem ab soluta distinctio. Placeat sequi omnis
+              maiores nemo recusandae, debitis harum nam magnam, ut dolore dolor
+              consequuntur corrupti nisi ad voluptates repellat! Tempore
+              assumenda aliquam sapiente odit, a quis debitis doloremque nobis
+              totam, similique, provident mollitia?
+              <br />
+              <br />
+              <div>
+                <a href="#">Terms of Service</a>
+              </div>
+              <br />
+              <br />
+              <div>
+                <a href="#">Privacy Policy</a>
+              </div>
+              <br />
+              <br />
+              <div>
+                <a href="#">Accessibility</a>
+              </div>
+            </div>
+          </div>
+        </li>
+      </div>
     </LoginLayout>
   );
 };
