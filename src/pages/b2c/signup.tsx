ba09fda87bf9b2ecf4164d7b5af30b38e3ae3298
sljico <anessljivo@gmail.com>
2023-02-23 12:07:13 +0100

    test

--- src/pages/b2c/signup.tsx
+++ src/pages/b2c/signup.tsx
@@ -56,9 +56,8 @@ export default function Login() {
             p.appendChild(paraTextNode);
 
             setTimeout(() => {
-              console.log('timeout ', document.getElementById('extension_termsOfUseContentWrapper'));
-              console.log('timeout2 ', document.getElementById('test'));
-            }, 2000);
+              console.log('timeout ', document.getElementById('test'))
+            }, 1000);
             console.log(tc)
             console.log(checkbox)
 
