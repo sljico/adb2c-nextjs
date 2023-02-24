d99813591d4de5cbab549fb83d7a20cc3dc60ced
sljico <anessljivo@gmail.com>
2023-02-24 14:41:45 +0100

    test

--- src/pages/b2c/social-login.tsx
+++ src/pages/b2c/social-login.tsx
@@ -70,6 +70,7 @@ export default function SocialLogin() {
         console.log(checkbox);
 
         detach(el);
+        forEach(checkbox, (item) => detach(item));
          
          
         //  insertBefore(el, checkbox);
