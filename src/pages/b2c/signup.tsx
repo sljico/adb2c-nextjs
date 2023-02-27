aa6d52f3dcf21cafdae627e9799a02c44064e36b
sljico <anessljivo@gmail.com>
2023-02-27 11:44:15 +0100

    test

--- src/pages/b2c/signup.tsx
+++ src/pages/b2c/signup.tsx
@@ -100,6 +100,44 @@ const Signup: NextPage<unknown> = () => {
           </li>
         </div>
       </LoginLayout>
+      <Script
+        id="test3"
+        dangerouslySetInnerHTML={{
+          __html: `
+        waitForElm('#extension_termsOfUseContentWrapper').then((el) => {
+          const checkbox = document.querySelector('ul > li.CheckboxMultiSelect');
+          const p = document.createElement('p');  
+          const paraTextNode = document.createTextNode('HIPPA authorization');
+          p.appendChild(paraTextNode);
+
+          // detach(el);
+          // insertBefore(el, checkbox);
+          // insertBefore(p, checkbox);
+        });
+
+        function waitForElm(selector) {
+          return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+              return resolve(document.querySelector(selector))
+            }
+
+            const observer = new MutationObserver(mutations => {
+              if (document.querySelector(selector)) {
+                resolve (document.querySelector(selector));
+                observer.disconnect();
+              }
+            });
+
+            observer.observe(document.body, {
+              childList: true,
+              subtree: true
+            })
+          });
+        }
+      
+      `,
+        }}
+      />
       <Script id="signup-script">
         {`
           waitForElm('#extension_termsOfUseContentWrapper').then((el) => {
