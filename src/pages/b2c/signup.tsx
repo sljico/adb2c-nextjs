795a6c52756150adba2fc12b48c6d847cc5aa251
sljico <anessljivo@gmail.com>
2023-02-27 18:57:48 +0100

    test

--- src/pages/b2c/signup.tsx
+++ src/pages/b2c/signup.tsx
@@ -106,7 +106,52 @@ const Signup: NextPage<unknown> = () => {
         </div>
       </LoginLayout>
       {/* <script type="text/javascript" src="/js/test2.js"></script> */}
-      <Script src="/js/test2.js" strategy="beforeInteractive"></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+      console.log('da');
+      waitForElm('#extension_termsOfUseContentWrapper').then((el) => {
+        const checkbox = document.querySelector('ul > li.CheckboxMultiSelect');
+        const p = document.createElement('p');  
+        const paraTextNode = document.createTextNode('HIPPA authorization');
+        p.appendChild(paraTextNode);
+
+        detach(el);
+        insertBefore(el, checkbox);
+        insertBefore(p, checkbox);
+      });
+
+      function waitForElm(selector) {
+        return new Promise(resolve => {
+          if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector))
+          }
+
+          const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+              resolve (document.querySelector(selector));
+              observer.disconnect();
+            }
+          });
+
+          observer.observe(document.body, {
+            childList: true,
+            subtree: true
+          })
+        });
+      }
+
+      function detach(el) {
+        return el?.parentNode.removeChild(el);
+      }
+
+      function insertBefore(newNode, referenceNode) {
+        referenceNode?.parentNode.insertBefore(newNode, referenceNode);
+      }
+      
+      `,
+        }}
+      />
       {/* <script src="../../test.js"></script> */}
       {/* <Script id="signup-script" strategy="beforeInteractive">
         {`
