d1d9e70eb3002d14bcb472bcc17d303ed01ebdfc
sljico <anessljivo@gmail.com>
2023-04-27 10:40:58 +0200

    test link

---
+++ src/pages/provider-portal/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function TestIndex() {
+  return (
+    <div>
+      <h1>TestIndex</h1>
+      <Link href="/privacy">Privacy</Link>
+    </div>
+  );
+}
